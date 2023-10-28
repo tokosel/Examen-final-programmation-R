860d96a328518e80f859e122b49a8d8580d1c8b5
tokosel <abdoulayesallboke@outlook.fr>
2023-10-28 16:22:08 +0000

    Structure des données

---
+++ projet_final_sen_ndawal_marketing.R
@@ -0,0 +1,7 @@
+# Chargez la bibliothèque readr
+library(readr)
+
+#1. Importation des données: Comment importeriez-vous le fichier CSV de marketing dans R? 
+ventesDf <- read.csv("C:/Users/hp/Desktop/corrected_sen_ndawal_marketing_data.csv")
+
+#Affichez les premières lignes du jeu de données pour vérifier que l'importation a réussi.
